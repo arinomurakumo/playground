4c73aa6089cf2193ec0c11b7e7afa5ef15ddc5fb
ariariasria <r.arimura@mtacreations.co.jp>
2022-08-04 01:43:52 +0900

    add: Export the test results automatically

--- src/storybook/build-webpack/.storybook/test-runner.js
+++ src/storybook/build-webpack/.storybook/test-runner.js
@@ -1,17 +1,36 @@
-const { injectAxe, checkA11y } = require('axe-playwright')
+const { injectAxe, getViolations } = require('axe-playwright')
+const fs = require('fs')
 
 module.exports = {
+  setup() {
+    fs.mkdir(
+      process.cwd() + '/__accessibility__/',
+      { recursive: true },
+      err => {
+        if (err) throw err
+      }
+    )
+  },
   // Axeをストーリーに注入し、レンダリングしたらアクセシビリティテストを実行
   async preRender(page, context) {
     await injectAxe(page)
   },
   async postRender(page, context) {
-    // ストーリーのルート要素から始めて、DOMツリーを横断して問題をチェックするようにAxeを設定
-    await checkA11y(page, '#root', {
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true
-      }
+    const violations = await getViolations(page, '#root', {
+      detailedReport: true
+    })
+
+    // Do something with violations
+    // For example, write them to a file
+    await new Promise((resolve, reject) => {
+      fs.writeFile(
+        process.cwd() + `/__accessibility__/${context.id}.json`,
+        JSON.stringify(violations, null, 2),
+        err => {
+          if (err) reject(err)
+          resolve()
+        }
+      )
     })
   }
 }
